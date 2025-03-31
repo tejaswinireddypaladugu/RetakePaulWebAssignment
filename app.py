40e0e5cf3d149d823c950dbe081c45e92d1437ab
tejaswinireddy989@gmail.com <tejaswinireddy989@gmail.com>
2025-03-31 18:57:45 +0530

    indexpage

--- app.py
+++ app.py
@@ -11,3 +11,10 @@ db = client.get_database('CarSellingProduct')
 app.static_folder='static'
 
 app.secret_key='1234'
+
+@app.route("/")
+def home():
+    if(session.get('user')):
+       print('user',session.get('user'))
+       return render_template('index.html',username=session.get('user'))
+    return render_template('index.html',username="")
